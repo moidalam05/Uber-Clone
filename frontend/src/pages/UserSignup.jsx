a26e0050ded63a0d30c30139bd24554805a07762
moidalam05 <moidalam005@hotmail.com>
2025-07-30 20:02:56 +0530

    implement UserSignup component with form handling and state management

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import uberLogo from "../assets/Uber-logo-B.png";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
 
 const UserSignup = () => {
+  const [userData, setUserData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(userData);
+    setUserData({
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="p-8 h-screen flex flex-col justify-between bg-gradient-to-tr from-[#f0f4ff] via-[#f8faff] to-white">
       <div>
         <img className="w-16 mb-10" src={uberLogo} alt="logo" />
-        <form>
+        <form onSubmit={(e) => submitHandler(e)}>
           <label className="flex text-base font-medium mb-2">
             What's your name
           </label>
@@ -21,15 +44,18 @@ const UserSignup = () => {
               name="firstname"
               placeholder="First name"
               autoComplete="given-name"
+              value={userData.firstname}
+              onChange={handleChange}
             />
             <input
               className="bg-[#f5f6fa] border border-gray-200 focus:outline-none focus:ring focus:ring-blue-100 rounded-md px-4 py-2 w-full text-base placeholder:text-sm"
               type="text"
-              required
               id="lastname"
               name="lastname"
               placeholder="Last name"
               autoComplete="given-name"
+              value={userData.lastname}
+              onChange={handleChange}
             />
           </div>
           <label htmlFor="email" className="flex text-base font-medium mb-2">
@@ -43,6 +69,8 @@ const UserSignup = () => {
             name="email"
             placeholder="email@example.com"
             autoComplete="email"
+            value={userData.email}
+            onChange={handleChange}
           />
           <label htmlFor="password" className="flex text-base font-medium mb-2">
             What's your password
@@ -55,6 +83,8 @@ const UserSignup = () => {
             name="password"
             placeholder="password"
             autoComplete="current-password"
+            value={userData.password}
+            onChange={handleChange}
           />
           <button className="bg-[#111] relative flex items-center justify-center text-white font-semibold rounded-md px-4 py-2 w-full text-lg mb-3">
             Signup{" "}
